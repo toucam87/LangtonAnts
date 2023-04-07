56ba84ebeab9428565149559f5d7fb8ccc2b528d
toucam87 <49007994+toucam87@users.noreply.github.com>
2023-04-07 13:24:36 +0200

    v0.1 first prototype
    painting and default color ability in, one ant at the center

--- Board/board_tilemap.gd
+++ Board/board_tilemap.gd
@@ -7,6 +7,7 @@ signal board_size_changed(size)
 enum {WHITE, BLACK, BLUE, RED, YELLOW, GREEN, ORANGE, PURPLE}
 
 var active_color = 1
+var default_color = 0
 var is_painting_tiles = true
 var old_mouse_pos = null
 
@@ -32,4 +33,10 @@ func _input(_event: InputEvent) -> void:
 			var map_point = local_to_map(to_local(point))
 			if get_cell_source_id(0, map_point) != -1:
 				swap_tile(map_point, active_color)
-		
+	elif Mouse.right_click_pressed == true:
+		var points_to_paint = Mouse.lerped_mouse_positions
+		points_to_paint.append(Mouse.mouse_pos)
+		for point in points_to_paint:
+			var map_point = local_to_map(to_local(point))
+			if get_cell_source_id(0, map_point) != -1:
+				swap_tile(map_point, default_color)
